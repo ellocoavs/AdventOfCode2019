9128cd30e3b988856d6a9a69fcdc1e242e858900
ellocoavs <el_loco_avs@hotmail.com>
2019-12-02 09:14:00 +0100

    Starting Day 2

---
+++ 2019/Day 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("input.txt");
+            string[] split = text.Split(",");
+            int[] parts= new int[1000] ;
+            int counter = 0;
+            foreach (string x in split)
+            {
+                parts[counter] =Int32.Parse(x);
+                counter++;
+            }
+                    
+        }
+    }
+}
