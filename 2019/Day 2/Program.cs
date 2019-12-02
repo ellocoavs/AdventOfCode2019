b3824ffc8e9904cba4c32358ba7e9ac23383b74b
jarnogit <jarno@snappet.org>
2019-12-02 13:59:28 +0100

    Worked, don't forget: replaces positions 1 and 2 with values 12 and 2

--- 2019/Day 2/Program.cs
+++ 2019/Day 2/Program.cs
@@ -16,27 +16,34 @@ namespace Day_2
                 opcodes[counter] =Int32.Parse(x);
                 counter++;
             }
-
-            for (int position = 0; opcodes[position] != 99; position += 4)
+            //int[] opcodes = {1,1,1,4,99,5,6,0,99}; //TESTING INPUTS
+            for (int position = 0; ; position += 4)  // removed middle condition opcodes[position] != 99 cause not needed
             {
+                Console.WriteLine("Opcode at position 0 is: " + opcodes[0]);
+                Console.WriteLine("Processing opcodes: " + opcodes[position] + "," + opcodes[position+1]+ "," + opcodes[position+2] + "," + opcodes[position+3]);
+                Console.WriteLine("Operands at: " + opcodes[position+1] + "," + opcodes[position+2] + " are: "+  opcodes[opcodes[position+1]] + "," + opcodes[opcodes[position+2]] );
+                
                 if (opcodes[position] == 1)
                 {
-
+                    opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] + opcodes[opcodes[position+2]];
+                    Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
                 }
-                if (opcodes[position] == 2)
+                else if (opcodes[position] == 2)
                 {
-
+                    opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] * opcodes[opcodes[position+2]];
+                    Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
                 }
-                if (opcodes[position] == 99)
+                else if (opcodes[position] == 99)
                 {
-
+                    Console.WriteLine("Code 99 encountered, stopping after seeing opcode 99 at: " + position);
+                    break;
                 }
                 else
                 {
                     Console.WriteLine("Invalid opcode detected: " + opcodes[position]);
                 }
             }
-
+            Console.WriteLine("Ended with value: " + opcodes[0] + " at position 0");
         }
     }
 }
