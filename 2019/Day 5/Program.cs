2e745cfb4e77e3879b7e29c9f0584ae2ec7c1c00
ellocoavs <el_loco_avs@hotmail.com>
2019-12-05 21:37:51 +0100

    Day 5 part 1 done

--- 2019/Day 5/Program.cs
+++ 2019/Day 5/Program.cs
@@ -5,6 +5,10 @@ namespace Day_5
 {
     class Program
     {
+        static class Globals
+        {
+            public static int input = 1;
+        }
         static void Main(string[] args)
         {
             var text = File.ReadAllText("input.txt");
@@ -20,66 +24,60 @@ namespace Day_5
             }
             Array.Copy(original,opcodes,100000);
 
-            int noun = 0;
-            int verb = 0;
             int result = 0 ;
-            while (noun < 100){
-                while (verb < 100){
-                    Console.WriteLine("Computing with verb, noun as: "+noun+","+verb);
-                    result = Compute(opcodes,noun,verb);
-                    if (result == 19690720)
-                    {
-                        Console.WriteLine("Input was " + (noun*100+verb));
-                        break;
-                    }
-                    else Array.Copy(original,opcodes,100000); verb++; //restart with original array and try again
-                }
-                if (result == 19690720){
-                    Console.WriteLine("Input was " + (noun*100+verb));
-                    break;
-                }
-                
-                else verb =0;Array.Copy(original,opcodes,100000); noun++;//restart with original array and try again
-            }
+            result = Compute(opcodes);   
+        }
             
             
             
            
-            
-        }
-        static int Compute (int[] opcodes, int noun, int verb) 
+        
+        static int Compute (int[] opcodes) 
         {
-            opcodes[1]= noun;
-            opcodes[2]= verb;
-            for (int position = 0; ; position += 1)  //edit to do +1 and do an extra +3 if needed
+            for (int position = 0; ; position +=2)  //edit to do +1 and do an extra +3 if needed
             {
-                //Console.WriteLine("Opcode at position 0 is: " + opcodes[0]);
+                int actualOpcode = opcodes[position] % 100; //last two digits
+                bool isPosMode1 = (opcodes[position] / 100) % 10 == 0; // digit before last two
+                bool isPosMode2 = (opcodes[position] / 1000) % 10 == 0; //digit before last three
+                bool isPosMode3 = (opcodes[position] / 10000) % 10 == 0; // digit before last four
+
+                Console.WriteLine("Opcode at position 0 is: " + actualOpcode);
+                Console.WriteLine("IsPositionalMode values are " + isPosMode1+isPosMode2+isPosMode3);
+                
                 //Console.WriteLine("Processing opcodes: " + opcodes[position] + "," + opcodes[position+1]+ "," + opcodes[position+2] + "," + opcodes[position+3]);
                 //Console.WriteLine("Operands at: " + opcodes[position+1] + "," + opcodes[position+2] + " are: "+  opcodes[opcodes[position+1]] + "," + opcodes[opcodes[position+2]] );
-                
-                if (opcodes[position] == 1)
+              
+
+                if (actualOpcode == 1) //ADD
                 {
-                    opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] + opcodes[opcodes[position+2]];
+                    //OLD WAY opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] + opcodes[opcodes[position+2]];
                     //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
-                    position += 3;
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : opcodes[position+2];
+                    opcodes[opcodes[position+3]] = parameter1 + parameter2;
+                    position += 2;
                 }
-                else if (opcodes[position] == 2)
+                else if (actualOpcode == 2) //Multiply
                 {
-                    opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] * opcodes[opcodes[position+2]];
+                    //opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] * opcodes[opcodes[position+2]];
                     //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
-                    position += 3;
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : opcodes[position+2];
+                    opcodes[opcodes[position+3]] = parameter1 * parameter2;
+                    position += 2;
                 }
-                else if (opcodes[position] == 3) //only one parameter, no +3 needed
+                else if (actualOpcode == 3) //only one parameter, no +3 needed. grab input from user and store at parameter
                 {
-                 
+                    opcodes[opcodes[position+1]] = Globals.input;
+                    Console.WriteLine("Storing value: " + Globals.input + " at position: " +  opcodes[position+1]);
                     //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
                 }
-                else if (opcodes[position] == 4)
+                else if (actualOpcode == 4) //print what's stored at parameter
                 {
-                 
+                    Console.WriteLine(opcodes[opcodes[position+1]]);
                     //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
                 }
-                else if (opcodes[position] == 99)
+                else if (actualOpcode == 99)
                 {
                     Console.WriteLine("Code 99 encountered, stopping after seeing opcode 99 at: " + position);
                     break;
