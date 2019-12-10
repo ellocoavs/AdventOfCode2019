e469b96b2adf1cf9ce8a7e793b41bf052d05b2bf
jarnogit <jarno@snappet.org>
2019-12-10 14:17:32 +0100

    day 8 part 2 done

--- 2019/Day 8/Program.cs
+++ 2019/Day 8/Program.cs
@@ -11,6 +11,8 @@ namespace Day_8
         {
             public static int width = 25;
             public static int height = 6;
+            // public static int width = 2;
+            // public static int height = 2;
         }
         static void Main(string[] args)
         {
@@ -24,13 +26,82 @@ namespace Day_8
                 layers.Add(input.Substring(i,(width*height)));
             }
 
-            int index = FindIndexOfLayerWithLeastZeroes(layers);
+            //int index = FindIndexOfLayerWithLeastZeroes(layers);
 
-            string layer = layers[index];
-            int answer = CalculateString(layer);
+            //string layer = layers[index];
+            //int answer = CalculateString(layer);
 
-            Console.WriteLine("Answer is: " +answer);
-
+            //Console.WriteLine("Answer is: " +answer);
+            int[,] image = new int[width,height];
+            for (int i =0; i<Globals.width;i++)
+            {
+                for (int j =0; j<Globals.height;j++)
+                {
+                    image[i,j] = 2;
+                }
+            }
+            PrintImage(image);
+            //fill in values with FindFirstNonTransparent
+            for (int i =0; i<Globals.width;i++)
+            {
+                for (int j =0; j<Globals.height;j++)
+                {
+                    if (image[i,j] ==2)
+                    {
+                        Console.WriteLine("Processing coords" + i +","+j);
+                        image[i,j] = FindFirstNonTransparent(layers,i,j);
+                    }
+                    
+                }
+            }
+            //print
+            PrintImage(image);
+        }
+        public static int FindFirstNonTransparent (List<string> layers,int x, int y){
+            
+            bool nontransparentFound = false;
+            int size = Globals.width*Globals.height;
+            int numberoflayers = layers.Count;
+            while (!nontransparentFound)
+            {
+                for(int i=0;i<numberoflayers;i++)
+                {
+                    string currentlayer =layers[i];
+                    int index = x + (y * Globals.width); //converting coordinates to indexposition
+                    //Console.WriteLine("Processing layer number" + i + " which looks like:" + currentlayer);
+                    //onsole.WriteLine("Processed character should be: " + currentlayer[index]);
+                    if(currentlayer[index] == '1' | currentlayer[index] == '0')
+                    {
+                        
+                        char result = currentlayer[index];
+                        int actualresult = (int)char.GetNumericValue(result);
+                        //Console.WriteLine("Returning colorvalue: " + actualresult);
+                        return actualresult;
+                    }
+                    
+                }
+            }
+            Console.WriteLine("Trying to return invalid color");
+            return 2;
+        }
+        public static void PrintImage (int[,] image)
+        {
+           // Console.BackgroundColor = ConsoleColor.Blue;
+            for (int j =0; j<Globals.height;j++)
+            {
+                for (int i =0; i<Globals.width;i++) //inner iteration = width so that we do entire horizontal lines first.
+                {
+                    if (image[i,j] == 0){
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    if (image[i,j] == 1){
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.Write("{0} ", image[i, j]);
+                }
+                Console.Write(Environment.NewLine);
+            }       
+            Console.ResetColor();
         }
         public static int FindIndexOfLayerWithLeastZeroes (List<string> layers)
         {
