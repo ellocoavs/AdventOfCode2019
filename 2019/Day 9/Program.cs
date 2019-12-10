30f6f1fc63c4389cb69caf06a96f3eb689103271
jarnogit <jarno@snappet.org>
2019-12-10 17:38:16 +0100

    day 9 work start

---
+++ 2019/Day 9/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+
+namespace Day_9
+{
+    class Program
+    {
+        static class Globals
+        {
+            public static Int64 input = 1; // 1 is test mode
+            public static Int64 relativeBase = 0;
+        }
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("testinput1.txt");
+            string[] split = text.Split(",");
+            //Int64[] original = new Int64[1000000000];
+            Int64[] opcodes= new Int64[50000000];
+            Int64 counter = 0;
+            
+            foreach (string x in split)
+            {
+                opcodes[counter] =Int64.Parse(x);
+                counter++;
+            }
+            //Array.Copy(original,opcodes,1000000);
+
+            Int64 result = 0 ;
+            result = Compute(opcodes);   
+        }
+            
+            
+            
+           
+        
+        static Int64 Compute (Int64[] opcodes) 
+        {
+            for (Int64 position = 0; ; )
+            {
+                Int64 actualOpcode = opcodes[position] % 100; //last two digits
+                bool isPosMode1 = (opcodes[position] / 100) % 10 == 0; // digit before last two
+                bool isPosMode2 = (opcodes[position] / 1000) % 10 == 0; //digit before last three
+                bool isPosMode3 = (opcodes[position] / 10000) % 10 == 0; // digit before last four
+                bool isRelMode1 = (opcodes[position] / 100) % 10 == 2; // digit before last two
+                bool isRelMode2 = (opcodes[position] / 1000) % 10 == 2; //digit before last three
+                bool isRelMode3 = (opcodes[position] / 10000) % 10 == 2; // digit before last four
+                //Console.WriteLine("Opcode at position 0 is: " + actualOpcode);
+                //Console.WriteLine("IsPositionalMode values are " + isPosMode1+isPosMode2+isPosMode3);
+                
+                //Console.WriteLine("Processing opcodes: " + opcodes[position] + "," + opcodes[position+1]+ "," + opcodes[position+2] + "," + opcodes[position+3]);
+                //Console.WriteLine("Operands at: " + opcodes[position+1] + "," + opcodes[position+2] + " are: "+  opcodes[opcodes[position+1]] + "," + opcodes[opcodes[position+2]] );
+              
+
+                if (actualOpcode == 1) //ADD 3 params
+                {
+                    //OLD WAY opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] + opcodes[opcodes[position+2]];
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : isRelMode1 ? opcodes[opcodes[position+1+Globals.relativeBase]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : isRelMode2 ? opcodes[opcodes[position+2+Globals.relativeBase]] : opcodes[position+2];
+                    opcodes[opcodes[position+3]] = parameter1 + parameter2;
+                    position += 4; //+4 because 1 op + 3 params
+                }
+                else if (actualOpcode == 2) //Multiply 3 params
+                {
+                    //opcodes[opcodes[position+3]] = opcodes[opcodes[position+1]] * opcodes[opcodes[position+2]];
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : isRelMode1 ? opcodes[opcodes[position+1+Globals.relativeBase]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : isRelMode2 ? opcodes[opcodes[position+2+Globals.relativeBase]] : opcodes[position+2];
+                    opcodes[opcodes[position+3]] = parameter1 * parameter2;
+                    position += 4;
+                }
+                else if (actualOpcode == 3) //only one parameter. grab input from user and store at parameter
+                {
+                    opcodes[opcodes[position+1]] = Globals.input;
+                    //Console.WriteLine("Storing value: " + Globals.input + " at position: " +  opcodes[position+1]);
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    position += 2;
+                }
+                else if (actualOpcode == 4) //print what's stored at parameter1
+                {
+                    Console.WriteLine(opcodes[opcodes[position+1]]);
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    position += 2;
+                }
+                else if (actualOpcode == 5) //jump-if-true, 2 params
+                {
+                    //Console.WriteLine(opcodes[opcodes[position+1]]);
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    //if param 1 true go to param 2 else position += 2;
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : isRelMode1 ? opcodes[opcodes[position+1+Globals.relativeBase]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : isRelMode2 ? opcodes[opcodes[position+2+Globals.relativeBase]] :opcodes[position+2];
+                    if (parameter1 > 0){position=parameter2;} else {position += 3;}
+                }
+                else if (actualOpcode == 6) //jump-if-false 2 params
+                {
+                    //Console.WriteLine(opcodes[opcodes[position+1]]);
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    // if param 1 fals go to param 2 else position += 2;
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : isRelMode1 ? opcodes[opcodes[position+1+Globals.relativeBase]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : isRelMode2 ? opcodes[opcodes[position+2+Globals.relativeBase]] : opcodes[position+2];
+                    if (parameter1 == 0){position=parameter2;} else {position += 3;}
+                }
+                else if (actualOpcode == 7) //less than, 3 params
+                {
+                    //Console.WriteLine(opcodes[opcodes[position+1]]);
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    // if param 1less than param2 -> 1 else -> 0
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : isRelMode1 ? opcodes[opcodes[position+1+Globals.relativeBase]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : isRelMode2 ? opcodes[opcodes[position+2+Globals.relativeBase]] : opcodes[position+2];
+                    opcodes[opcodes[position+3]] = parameter1 < parameter2 ? 1  : 0;
+                    position += 4;
+                }
+                else if (actualOpcode == 8) //equals, 3 params
+                {
+                    //Console.WriteLine(opcodes[opcodes[position+1]]);
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    // if param1 equals param2 -> 1 else 0
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : isRelMode1 ? opcodes[opcodes[position+1+Globals.relativeBase]] : opcodes[position+1];
+                    var parameter2 = isPosMode2 ? opcodes[opcodes[position+2]] : isRelMode2 ? opcodes[opcodes[position+2+Globals.relativeBase]] : opcodes[position+2];
+                    opcodes[opcodes[position+3]] = parameter1 == parameter2 ? 1 : 0;
+                    position += 4;
+                }
+                else if (actualOpcode == 9) //adjust relative base, 1 params
+                {
+                    //Console.WriteLine(opcodes[opcodes[position+1]]);
+                    //Console.WriteLine("Result stored at position: " + position + " is: " + opcodes[position+3]);
+                    // if param1 equals param2 -> 1 else 0
+                    var parameter1 = isPosMode1 ? opcodes[opcodes[position+1]] : isRelMode1 ? opcodes[opcodes[position+1+Globals.relativeBase]] : opcodes[position+1];
+                    Globals.relativeBase += parameter1;
+                }
+                else if (actualOpcode == 99)
+                {
+                    Console.WriteLine("Code 99 encountered, stopping after seeing opcode 99 at: " + position);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid opcode detected: " + opcodes[position]);
+                }
+
+            }
+            return opcodes[0];
+        }
+    }
+}
