1e0beb01f3a96a0943c506accfbd08354be452ed
ellocoavs <el_loco_avs@hotmail.com>
2019-12-21 13:42:51 +0100

    create a class?

--- 2019/Day 14/Program.cs
+++ 2019/Day 14/Program.cs
@@ -10,11 +10,29 @@ namespace Day_14
         {
             var rules = File.ReadAllLines("input.txt");
             
-            List<(int,string)[]> leftsides = new List<(int,string)[]>();
-            List<(int,string)> rightsides = new List<(int,string)>();
+            List<(int,string)[]> inputchems = new List<(int,string)[]>();
+            List<(int,string)> outputchems = new List<(int,string)>();
+
+            foreach (string rule in rules)
+            {
+                var rightsideofrule = rule.Substring(rule.LastIndexOf("=>") + 1);
+                Console.WriteLine(rightsideofrule);
+                var leftsideofrule = rule.Substring(0, rule.LastIndexOf("=>"));
+                Console.WriteLine(leftsideofrule);
+                
+                // inputchems.Add(leftsideofrule);
+                // outputchems.Add(rightsideofrule);
+            }
 
             string fuel = "FUEL";
             string ore = "ORE";
+
+            
+        }
+
+        static (int,string)[] FuelToOre (int count,string chemical)
+        {
+            return (0,"");//create rulepoin class with a count and name?
         }
     }
 }
