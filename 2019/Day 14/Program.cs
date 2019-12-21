eecdb55a38066b9f2806065b5f8c4072756e5f34
ellocoavs <el_loco_avs@hotmail.com>
2019-12-21 12:45:01 +0100

    day 14 setting up

---
+++ 2019/Day 14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rules = File.ReadAllLines("input.txt");
+            
+            List<(int,string)[]> leftsides = new List<(int,string)[]>();
+            List<(int,string)> rightsides = new List<(int,string)>();
+
+            string fuel = "FUEL";
+            string ore = "ORE";
+        }
+    }
+}
