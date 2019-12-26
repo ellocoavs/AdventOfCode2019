917e1cffd4652f843468257d887fa0fa19a986dd
ellocoavs <el_loco_avs@hotmail.com>
2019-12-26 13:25:38 +0100

    First two test cases work, 3 and 4 don't. Likely due to leftovers? Add list for keep track possibly?

--- 2019/Day 14/Program.cs
+++ 2019/Day 14/Program.cs
@@ -9,9 +9,12 @@ namespace Day_14
         static void Main(string[] args)
         {
             //var rules = File.ReadAllLines("input.txt");
-            var rules = File.ReadAllLines("testinput1.txt");
+            var rules = File.ReadAllLines("testinput3.txt");
             List<List<(int,string)>> inputchems = new List<List<(int,string)>>();
             List<(int,string)> outputchems = new List<(int,string)>();
+            List<string> visited = new List<string>();
+            List<(int,string)> queue = new List<(int,string)>();
+
 
             foreach (string rule in rules)
             {
@@ -30,19 +33,20 @@ namespace Day_14
             }
 
             //some checks
-            bool check = (outputchems.Count) == inputchems.Count;
-            Console.WriteLine("Number of inputs matches number of outputs is: " +check);
-            for (int i=0; i<outputchems.Count; i++)
-            {
-                Console.WriteLine("input: "+string.Join("",inputchems[i]) + " => outputs: " + outputchems[i] );    
-            }
+            // bool check = (outputchems.Count) == inputchems.Count;
+            // Console.WriteLine("Number of inputs matches number of outputs is: " +check);
+            // for (int i=0; i<outputchems.Count; i++)
+            // {
+            //     Console.WriteLine("input: "+string.Join("",inputchems[i]) + " => outputs: " + outputchems[i] );    
+            // }
 
 
             string fuel = "FUEL";
             string ore = "ORE";
 
-            int answer =CalculateOreFromFuel((1,fuel),outputchems,inputchems);
-
+            //int answer =CalculateOreFromFuel((1,fuel),outputchems,inputchems);
+            queue.Add((1,fuel));
+            int answer = CalcOreBFS(outputchems,inputchems,visited, queue);
             Console.WriteLine("Result number of ore needed is: " + answer);
             
         }
@@ -55,6 +59,52 @@ namespace Day_14
             return (amount,name);
         }
 
+        static int CalcOreBFS(List<(int,string)> outputchems, List<List<(int,string)>> inputchems, List<string> visited, List<(int,string)> queue)
+        {
+            int OreCounter = 0;
+            while (queue.Count > 0)
+            {
+                Console.WriteLine("Top item in queue: " + queue[0]);
+                
+                //use a list as a queue, we need to be able to edit items inthe queue so no real queue datatype
+                var item = queue[0];
+                queue.RemoveAt(0);
+                
+                if(item.Item2 == "ORE") //if we ended up at ore we can count numbers of ore...
+                {
+                    Console.WriteLine("Ore found, adding "+ item.Item1 + " ore to total orecount of: " + OreCounter);
+                    OreCounter += item.Item1;
+                }
+                else
+                {
+                    //add children with right amount in queue, do look in queue to see if we have some identical stuffs queued?
+                    int ruleIndex = 0; 
+                    ruleIndex =  outputchems.FindIndex(a => a.Item2.Contains(item.Item2)); //find child items index in rule list
+                    foreach ((int,string)newchem in inputchems[ruleIndex]) //for each child item
+                    {
+                        //calculate amount of item we need
+                        int numbertouse = (int)(Math.Ceiling((double)item.Item1/(outputchems[ruleIndex].Item1))) * newchem.Item1;
+                        
+                        int foundinqueueindex = queue.FindIndex(a => a.Item2.Equals(newchem.Item2)); //find if child is queueud
+                        //if not in queue yet add as new with calculated amount
+                        if (foundinqueueindex== -1)
+                        {
+                            Console.WriteLine("Not found, adding to queue: "+numbertouse +" of item " +newchem.Item2);
+                            queue.Add((numbertouse,newchem.Item2));
+                        }
+                        //if in queue, add calculated amount to existing
+                        else
+                        {
+                            Console.WriteLine("Found at: "+ foundinqueueindex + " Adding amount to currently queued chem: "+numbertouse + " " + newchem.Item2);
+                            queue[foundinqueueindex] = (numbertouse+queue[foundinqueueindex].Item1, newchem.Item2);
+                        }
+                        //result += CalculateOreFromFuel((numbertouse,newchem.Item2),outputchems,inputchems);
+                    }
+
+                }
+            }
+            return OreCounter;
+        }
         static int CalculateOreFromFuel ((int,string) currentchem, List<(int,string)> outputchems, List<List<(int,string)>> inputchems)
         {
 
